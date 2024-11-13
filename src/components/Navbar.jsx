d9db189790352f37c14cefc660722254ecf6aba6
Takmim00 <takmimm@gmail.com>
2024-11-13 21:40:09 +0600

    navbar adde

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import userIcon from "../assets/user.png";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center">
+      <div></div>
+      <div className="space-x-5 text-gray-500">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/career">Career</Link>
+      </div>
+      <div className="flex gap-3 items-center">
+        <div className="">
+          <img src={userIcon} alt="" />
+        </div>
+        <button className="btn btn-neutral rounded-none">Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/layouts/HomeLayouts.jsx
+++ src/layouts/HomeLayouts.jsx
@@ -1,20 +1,22 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
-
+import Navbar from "../components/Navbar";
 
 const HomeLayouts = () => {
-    return (
-        <div>
-            <header>
-                <Header></Header>
-                <section className="w-11/12 mx-auto">
-                    <LatestNews></LatestNews>
-                </section>
-            </header>
-            <nav></nav>
-            <main></main>
-        </div>
-    );
+  return (
+    <div className="font-poppins">
+      <header>
+        <Header></Header>
+        <section className="w-11/12 mx-auto">
+          <LatestNews></LatestNews>
+        </section>
+      </header>
+      <nav className="w-11/12 mx-auto py-4">
+        <Navbar></Navbar>
+      </nav>
+      <main className="w-11/12 mx-auto"></main>
+    </div>
+  );
 };
 
 export default HomeLayouts;
